d386a19c8cc9deb508e46d4613edfe6279415c94
Sawyer-li <13894705977@163.com>
2018-01-11 09:57:16 +0800

    添加团队成员

--- server/app.js
+++ server/app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
 app.use(express.static('../web'));
-app.listen(8070,function(){
+app.listen(80,function(){
 	console.log("listen to 80");
 });
