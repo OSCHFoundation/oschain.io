27d9ada6f4b3df08d7d245c75fd3c304d87c36d0
Sawyer-li <13894705977@163.com>
2018-01-18 17:37:37 +0800

    修改字号

--- web/static1/js/index.js
+++ web/static1/js/index.js
@@ -1,3 +1,19 @@
+function browserRedirect(){
+	var sUserAgent = navigator.userAgent.toLowerCase();
+	var bIsIpad = sUserAgent.match(/ipad/i) == 'ipad';
+	var bIsIphone = sUserAgent.match(/iphone os/i) == 'iphone os';
+	var bIsMidp = sUserAgent.match(/midp/i) == 'midp';
+	var bIsUc7 = sUserAgent.match(/rv:1.2.3.4/i) == 'rv:1.2.3.4';
+	var bIsUc = sUserAgent.match(/ucweb/i) == 'web';
+	var bIsCE = sUserAgent.match(/windows ce/i) == 'windows ce';
+	var bIsWM = sUserAgent.match(/windows mobile/i) == 'windows mobile';
+	var bIsAndroid = sUserAgent.match(/android/i) == 'android';
+	if(bIsIpad || bIsIphone || bIsMidp || bIsUc7 || bIsUc || bIsCE || bIsWM || bIsAndroid ){
+		return true;
+	}else{
+		return false;
+	}
+}
 $(function(){
 	$('.col-md-1-5').mouseover(function(event) {
 		$(this).find('.model').show();
@@ -6,4 +22,7 @@ $(function(){
 		$(this).find('.model').hide();
 	});
 	$('.videobox video').css('height',$(window).height());
+	if(browserRedirect()){
+		document.getElementById("videojs").controls = true;
+	}
 })
