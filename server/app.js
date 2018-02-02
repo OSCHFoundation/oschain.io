23f521f03fa638d5bcbe08e29fc57b9f6d32a57d
Sawyer-li <13894705977@163.com>
2018-02-02 17:20:26 +0800

    local file

--- server/app.js
+++ server/app.js
@@ -4,6 +4,10 @@ var compression = require('compression')
 const app = express();
 app.use(compression());
 app.use(express.static('../web'));
+/*app.use(function(req,res,next){
+	console.log('aaa');
+	next();
+})*/
 app.listen(80,function(){
 	console.log("listen to 80");
 });
