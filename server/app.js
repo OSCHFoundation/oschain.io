2f0b26749bea11a6576a42aaf8ac8d13de843e15
Sawyer-li <13894705977@163.com>
2018-02-02 16:57:13 +0800

    use local video

--- server/app.js
+++ server/app.js
@@ -1,5 +1,8 @@
 const express = require('express');
+var compression = require('compression')
+
 const app = express();
+app.use(compression());
 app.use(express.static('../web'));
 app.listen(80,function(){
 	console.log("listen to 80");
